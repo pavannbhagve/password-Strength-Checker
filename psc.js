ada24acd34c063e50e4a2f58685945a593261e76
PB <pb@gmail.com>
2024-12-30 17:37:21 +0530

    Project start with html csss js

---
+++ psc.js
@@ -0,0 +1,43 @@
+const input = document.querySelector("#password");
+const output = document.querySelector("#output");
+
+
+
+input.addEventListener('input' , function () {
+
+    console.log(input.value);
+    
+
+ let password = input.value;
+
+ if(password.length < 8){
+
+    output.innerText = "Password is too short";
+    output.style.color ="red";
+//  }else{
+// output.innerText = "Pasword is too long"
+// output.style.color = "Green";
+
+ }
+
+ if (password.search(/[a-z]/) == -1) {
+
+    output.innerText = "Password is missing a lowarcase letter";
+    output.style.color = "red";
+    
+ }else if (password.search(/[A-Z]/) == -1){
+
+    output.innerText = "Password is missing a Uppercase letter";
+    output.style.color = "red";
+ }else if (password.search(/[0-9]/) == -1){
+
+    output.innerText = "Password is missing a Number letter";
+    output.style.color = "red";
+ }else{
+
+    output.innerText = "Password is strong";
+    output.style.color = "green";
+ }
+    
+    
+});
